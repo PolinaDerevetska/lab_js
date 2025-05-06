1f45d569f7fde897c6aed00e53d6e155bbb77b83
PolinaDerevetska <pauline.deretska@krfk.kai.edu.ua>
2025-05-06 12:41:51 +0300

    Add files via upload

---
+++ lab_8/add_script.js
@@ -0,0 +1,7 @@
+/* Створи функцію getFirstCharacter,
+яка приймає рядок і повертає його перший символ. */
+
+function getFirstCharacter(str) {
+   return str[0];
+}
+console.log(getFirstCharacter("Текст для перевірки")); 
